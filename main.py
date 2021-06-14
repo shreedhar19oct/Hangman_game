94f23fd8d9f071cd271db6f4f4fe8affd0ebc50c
shreedhar19oct <73355893+shreedhar19oct@users.noreply.github.com>
2021-06-14 21:18:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -38,6 +38,7 @@ while not end_of_game:
         if lives == 0:
             end_of_game = True
             print("You lose.")
+            print(f"The solution is {chosen_word}.")
 
     print(f"{' '.join(display)}")
 
